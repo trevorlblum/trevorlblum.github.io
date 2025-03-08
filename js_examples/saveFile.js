a04add7f32587bd7b5a19ed5cca048220603030f
Trevor Blum <trevorblumweb@dodo.com.au>
2025-03-08 17:21:32 +1100

    update js_examples

---
+++ js_examples/saveFile.js
@@ -0,0 +1,13 @@
+async function saveFile() {
+  // create a new handle
+  const newHandle = await window.showSaveFilePicker();
+
+  // create a FileSystemWritableFileStream to write to
+  const writableStream = await newHandle.createWritable();
+
+  // write our file
+  await writableStream.write("This is my file content");
+
+  // close the file and write the contents to disk.
+  await writableStream.close();
+}
